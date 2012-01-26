2e90c37d364d0dfcde790267100955663833b6d2
Eric Riemers <riemers@binkey.nl>
2012-01-26 14:36:23 +0100

    Added replay crawel for youtube

---
+++ replay/README.md
@@ -0,0 +1,21 @@
+Replay Crawler for youtube
+=========================================
+
+These files will help with the crawling of youtube. Server have a specific matchid per map round. We use this matchid to search on youtube for a specific field to match the movie to one of the servers in the list of SSMS.
+
+Install
+--------
+
+Make sure you install the correct tables (should be in the main .sql file of ssms)
+For the servers in question (default is no) turn the replay to "on" in the configuration tab of the server.
+
+	Please note, if your sv_tags doesn't include "replays" it will not show this option!
+
+Add a crontab entry to run every 2 minutes. It will check multiple times up to the duration setup in the mani configuration
+in ssms. If you set this any higher then 30 days it will stress out more to youtube and perhaps be faulty too (use on your on risk)
+
+Example crontab:
+
+	*/2 * * * * cd /home/lethal/public_html/admin/replay && php crawler.php > /dev/null 2>&1
+
+Please note that this needs the Zend package with the google library in order to work (thats why it is included)
---
+++ replay/crawler.php
@@ -0,0 +1,50 @@
+<?php
+	
+	// EDIBLES, NOM NOM
+	define( "CRAWLER_DELAY", 60 * 60 * 24 * 2 ); // delay in seconds before a new match is first crawled 
+	define( "CRAWLER_RANGE", 60 * 2 ); // range in seconds of the matches a crawl will cover
+	define( "CRAWLER_REPEAT_PERIOD", 60 * 60 * 24 * 7 ); // period in seconds it will look back to recrawl matches
+	define( "CRAWLER_REPEAT_COUNT", 4 ); // amount of previous periods it will recrawl matches of
+	
+	require_once 'Zend/Loader.php';
+	Zend_Loader::loadClass('Zend_Gdata_YouTube');
+	
+	mysql_connect( "www.lethal-zone.eu", "ssms", "NtZ33QfUveZNnUzC" ) or die( mysql_error() );
+	mysql_select_db( "ssms" ) or die( mysql_error() );
+	
+	$period = time() - CRAWLER_DELAY;
+	
+	$crawl_string = '';
+	for( $i = 0; $i < ( CRAWLER_REPEAT_COUNT + 1 ); $i++ )
+		$crawl_string .= ( $i == 0 ? '' : ' OR ' ) . 'matchdate BETWEEN FROM_UNIXTIME(' . ( $period - CRAWLER_REPEAT_PERIOD * $i - CRAWLER_RANGE ) . ") AND FROM_UNIXTIME(" . ( $period - CRAWLER_REPEAT_PERIOD * $i ) . ")";
+	
+	$matches = array();
+	$result = mysql_query( "SELECT * FROM matchids WHERE " . $crawl_string . " LIMIT 0, 10" );
+	while( $row = mysql_fetch_array( $result ) )
+		if( $row[ 'sessionid' ] > 1000000 )
+			$matches[ "match_" . strtoupper( dechex( $row[ 'sessionid' ] ) ) ] = array( $row[ 'serverid' ], $row[ 'mapname' ], $row[ 'matchdate' ] );
+	
+	echo 'Crawling ' . count( $matches ) . ' match(es)<br/>';
+	$matchlist = implode( ' | ', array_keys( $matches ) ); 
+	
+	if( empty( $matchlist ) ) Die();
+	
+	echo $matchlist . "<br/>";
+	$yt = new Zend_Gdata_YouTube();
+	$yt->setMajorProtocolVersion( 2 );
+	
+	$query = $yt->newVideoQuery();
+	$query->setVideoQuery( $matchlist );
+	$query->setMaxResults( 50 );
+
+	$videoFeed = $yt->getVideoFeed( $query->getQueryUrl( 2 ) );
+	
+	foreach ( $videoFeed as $videoEntry ) {
+		preg_match( "/match_([0-9a-f]{6,})/", implode( "\n", $videoEntry->getVideoTags() ), $matchid );
+		$authobj = $videoEntry->getAuthor();
+		$matchinfo = $matches[ 'match_' . strtoupper( $matchid[ 1 ] ) ];
+		mysql_query( "INSERT INTO videos ( youtubeid, youtubeuser, map, sessionid, matchdate, serverid, duration, title ) VALUES ( '" . $videoEntry->getVideoId() . "', '" . $authobj[ 0 ]->getName() . "', '" . $matchinfo[ 1 ] . "', '" . hexdec( $matchid[ 1 ] ) . "', '" . $matchinfo[ 2 ] . "', '" . $matchinfo[ 0 ] . "', '" . $videoEntry->getVideoDuration() . "', '" . $videoEntry->getVideoTitle() . "' )" );
+		echo mysql_error();
+	}
+	
+ ?>
